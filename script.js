668ad80e4f724a7a180178d2fd64d77cad8c2d50
anonymousapparation <116913613+anonymousapparation@users.noreply.github.com>
2023-07-04 15:20:20 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+    var enteredPassword = document.getElementById("passwordInput").value;
+    if (enteredPassword === "password") {
+        document.getElementById("message").textContent = "Congratulations! You entered the correct password.";
+    } else {
+        document.getElementById("message").textContent = "Sorry, the password you entered is incorrect.";
+    }
+}
