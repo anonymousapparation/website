2884edfb5c0c1268820b210aa8a53487caf7bc3e
anonymousapparation <116913613+anonymousapparation@users.noreply.github.com>
2023-07-04 16:24:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ function checkPassword() {
     var message = document.getElementById("message");
     
     if (enteredPassword === "crime") {
-        message.textContent = "A dark crime has been committed...";
+        var sentence = "A dark crime has been committed. You have been tasked to solve it.";
+        typeOutText(sentence, message);
     } else {
-        message.textContent = "DUDE WTF";
+        var sentence = "DUDE WTF";
+        typeOutText(sentence, message);
     }
+}
+
+function typeOutText(sentence, element) {
+    element.textContent = ""; // Clear the existing content
+    var words = sentence.split(" ");
+    var i = 0;
+  
+    var timer = setInterval(function() {
+        element.textContent += words[i] + " ";
+        i++;
+        if (i >= words.length) {
+            clearInterval(timer);
+        }
+    }, 200); // Adjust the typing speed by changing the delay (in milliseconds)
 }
 
 function checkEnter(event) {
